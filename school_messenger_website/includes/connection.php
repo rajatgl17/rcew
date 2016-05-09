4b92c31564e399326ab292a48db153989283567c
Rajat Goel <rajatgl17@gmail.com>
2016-05-09 18:00:15 +0530

    Update connection.php

--- school_messenger_website/includes/connection.php
+++ school_messenger_website/includes/connection.php
@@ -1,6 +1,6 @@
 <?php
 	try{
-		$pdo=new PDO('mysql:host=localhost;dbname=raja7466_sm','raja7466_sm','raja7466_sm');	
+		$pdo=new PDO('mysql:host=localhost;dbname=raja7466_sm','root','');	
 	} catch(PDOException $e) {
 		exit('Database connection error.');
 	}
